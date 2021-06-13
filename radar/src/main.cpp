948aa80e55c21201d6b752856bea7d25b809f24d
Dmitriy Potter <dmitriypottero@gmail.com>
2021-06-13 15:27:05 +0500

    different approach to radar

--- radar/src/main.cpp
+++ radar/src/main.cpp
@@ -1,74 +1,44 @@
 #include <Arduino.h>
-#include <Stepper.h>
 #include <Ultrasonic.h>
+#include <Servo.h>
 
-byte b1 = A0;
-byte b2 = A1;
-byte b3 = A5;
+#define SERVO_PIN 5
+#define TRIG_PIN 2
+#define ECHO_PIN 3
 
-bool direction = true;
-bool rotation = true;
-bool manual = true;
+unsigned long previousTime;
+bool taskComplete = false;
+int count = 0;
+int servoSpeed = 10;
 
-int stepsPerRevolution = 2048;
-
-byte echoPin1 = 2;
-byte trigPin1 = 3;
-
-byte echoPin2 = 6;
-byte trigPin2 = 5;
-
-Stepper stepper = Stepper(stepsPerRevolution, 11, 9, 10, 8);
-
-Ultrasonic ultrasonic1(trigPin1, echoPin1, 40000UL);
-Ultrasonic ultrasonic2(trigPin2, echoPin2, 40000UL);
-
-void getUltrasonicData(Ultrasonic ultrasonic1, Ultrasonic ultrasonic2, bool rotation) {
-    Serial.print(ultrasonic1.read());
-    Serial.print(" ");
-    Serial.print(ultrasonic2.read());
-    Serial.print(" ");
-    Serial.println(rotation);
-}
+Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN, 40000UL);
+Servo servo;
 
 void setup() {
-    pinMode(b1, INPUT_PULLUP);
-    pinMode(b2, INPUT_PULLUP);
-    pinMode(b3, INPUT_PULLUP);
-    stepper.setSpeed(5);
+    servo.attach(SERVO_PIN);
+    previousTime = millis();
 
     Serial.begin(9600);
 }
 
 void loop() {
-    if (digitalRead(b1) == 0) {
-        direction = true;
-    }
+    while (count < 180) {
+        if (millis() - previousTime > servoSpeed) {
+            servo.write(count);
+            previousTime = millis();
+            count++;
 
-    if (digitalRead(b2) == 0) {
-        direction = false;
-    }
-
-    if (digitalRead(b3) == 0) {
-        manual = false;
-    }
-
-    if (!manual) {
-        for (int i = 0; i < stepsPerRevolution / 2; i++) {
-            stepper.step(1);
-            rotation = true;
-
-            getUltrasonicData(ultrasonic1, ultrasonic2, rotation);
-        }
-
-        for (int i = 0; i < stepsPerRevolution / 2; i++) {
-            stepper.step(-1);
-            rotation = false;
-
-            getUltrasonicData(ultrasonic1, ultrasonic2, rotation);
+            Serial.println(ultrasonic.read());
         }
     }
 
-    if (direction && manual) stepper.step(1);
-    if (!direction && manual) stepper.step(-1);
+    while (count > 0) {
+        if (millis() - previousTime > servoSpeed) {
+            servo.write(count);
+            previousTime = millis();
+            count--;
+
+            Serial.println(ultrasonic.read());
+        }
+    }
 }
